7cfe99b26a8295b5f1938517bcb9dfd2eedc6519
Anson Francis <57131347+ansonfamps007@users.noreply.github.com>
2022-07-28 22:54:36 +0530

    Initial Commit

---
+++ src/main/java/com/library/user/libraryuser/LibraryUserApplication.java
@@ -0,0 +1,13 @@
+package com.library.user.libraryuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/user/libraryuser/LibraryUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.user.libraryuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
